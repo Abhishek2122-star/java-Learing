b1ae7cf6e8d31fcac2e4258edc7baea6104acc1c
Abhishek2122-star <abhisheklad963@gmail.com>
2025-12-30 22:49:55 +0530

    condition done

---
+++ condition.java
@@ -0,0 +1,12 @@
+public class condition {
+    public static void main(String[] args) {
+        int age = 18;
+        if (age >= 18){
+            System.out.println("eligeble for vote");
+        }
+        else{
+            System.out.println("not eligeble");
+        }
+    }
+    
+}
