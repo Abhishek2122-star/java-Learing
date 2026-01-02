a516d7b979a693887caedd00fa6c4bf9b844b3f2
Abhishek2122-star <abhisheklad963@gmail.com>
2026-01-02 23:28:45 +0530

    class and object done

---
+++ main.java
@@ -0,0 +1,42 @@
+// public class Parameterized {
+//     static void myMethod(String name){
+//         System.out.println(name + " " +"hello");
+//     }
+//     public static void main(String[] args) {
+//         myMethod( "Abhi");
+//         myMethod( "om");
+//     }
+    
+// }
+
+
+// multiparameterized parameter 
+
+// public class multiparameterized{
+//     static void multiparameter(String name , int age ){
+//         System.out.println(name + " is " + age );
+//     }
+//     public static void main(String[] args) {
+//         multiparameter("Abhi", 21);
+//         multiparameter("om", 18);
+//         multiparameter("raj", 21);
+//     }
+        
+// };
+
+
+// the realtime example with the using of if else condition for checking the age 
+
+public class main{
+    static void checkAge(int age){
+        if (age > 18){
+            System.out.println("eligeble for vote");
+        }
+        else{
+            System.out.println("not eligeble");
+        }
+    }
+    public static void main(String[] args) {
+        checkAge(17);
+    }
+};
---
+++ oops.java
@@ -0,0 +1,53 @@
+// public class oops {
+//     String x = "encapsulation";
+
+//     public static void main(String[] args) {
+//         oops  concept = new oops();
+//         System.out.println(concept.x);
+//     }
+// };
+
+// so do for student class 
+
+
+// public class Student{
+//     String A = "Abhi" ;
+//     String B = "b" ;
+
+//     public static void main(String[] args) {
+//         Student s1 = new Student();
+//         System.out.println(s1.B);
+//     }
+// };
+
+
+
+
+
+
+
+// for accessing the student marks 
+
+// public class Student {
+//     int s1marks = 50;
+//     int s2marks = 34;
+//     int s3marks = 35;
+
+//     public static void main(String[] args) {
+//         Student marks = new Student();
+//         System.out.println(marks.s1marks);
+//     }
+// }
+
+
+// for creating multiple class in single code
+
+public class oops{
+    int x = 5 ;
+}
+class Student {
+    public static void main(String[] args) {
+        oops concept = new oops();
+        System.out.println(concept.x);
+    }
+}
