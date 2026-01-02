5dc9f3ec0ec6b99fc746b7fd2f61d48e5e739fac
Abhishek2122-star <abhisheklad963@gmail.com>
2026-01-02 16:40:38 +0530

    Parameterized cover

---
+++ Parameterized.java
@@ -0,0 +1,10 @@
+public class Parameterized {
+    static void myMethod(String name){
+        System.out.println(name + " " +"hello");
+    }
+    public static void main(String[] args) {
+        myMethod( "Abhi");
+        myMethod( "om");
+    }
+    
+}
